db1e7e5ecff3f8ad69b53b6ec561e1b81b9c25ab
Hora de Picar <cvasconezm@gmail.com>
2022-03-30 21:42:51 -0500

    Login front

---
+++ pages/dashboard.jsx
@@ -0,0 +1,7 @@
+export const dashboard = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        'xl':'0 20px 13px rgba(0, 0, 0, 0.25)',
+        '3xl': '0 35px 35px rgba(0, 0, 0, 0.5)',
+        '4xl': [
+            '0 35px 35px rgba(0, 0, 0, 0.5)',
+            '0 45px 65px rgba(0, 0, 0, 0.3)'
+        ]
+      }
+    },
+    borderRadius:{
+      'none': '0',
+      'sm': '0.125rem',
+      DEFAULT: '0.25rem',
+      'md': '0.375rem',
+      'lg': '0.5rem',
+      'full': '9999px',
+      'large': '2rem'
+    }
   },
   plugins: [],
 }
